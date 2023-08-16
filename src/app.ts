08079c12e2ecd16f15dc26fcaa3f27b0b96b1e79
tearcode89 <sjun8932.3@gmail.com>
2023-08-16 12:27:39 +0900

    7. 고양이 데이터 모킹하기

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,11 @@
 import * as express from 'express';
+import { Cat, CatType} from "./app.model";
 
 const app: express.Express = express();
 const port: number = 9000;
 
 app.get('/', (req: express.Request, res: express.Response) => {
-    console.log(req)
-    res.send({ name: 'Jun', age: 256, friends: ['피카츄', '라이츄', '파이리', '꼬부기'] })
-})
-
-app.post('/test', (req,res) => {
-    res.send({person: 'KangIn Lee'})
+    res.send({ cats: Cat })
 })
 
 app.listen(port, () => {
