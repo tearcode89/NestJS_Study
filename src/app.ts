2ccab7913ed5355991688eee3c26119c862b3c46
tearcode89 <sjun8932.3@gmail.com>
2023-08-16 12:13:22 +0900

    6. Postman 설치

--- src/app.ts
+++ src/app.ts
@@ -8,6 +8,10 @@ app.get('/', (req: express.Request, res: express.Response) => {
     res.send({ name: 'Jun', age: 256, friends: ['피카츄', '라이츄', '파이리', '꼬부기'] })
 })
 
+app.post('/test', (req,res) => {
+    res.send({person: 'KangIn Lee'})
+})
+
 app.listen(port, () => {
     console.log(`예제의 앱은 http://localhost:${port}를 구동하고 있어요~ 🤞`)
 })
