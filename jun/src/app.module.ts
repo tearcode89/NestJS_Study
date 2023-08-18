b96910240a0ce9fa153f998b94effa589aaa896c
tearcode89 <sjun8932.3@gmail.com>
2023-08-18 18:43:40 +0900

    25. Nest Middleware

--- jun/src/app.module.ts
+++ jun/src/app.module.ts
@@ -1,12 +1,17 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CatsModule } from './cats/cats.module';
 import { UsersModule } from './users/users.module';
+import { LoggerMiddleware } from './logger/logger.middleware';
 
 @Module({
   imports: [CatsModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer): any {
+    consumer.apply(LoggerMiddleware).forRoutes('*');
+  }
+}
